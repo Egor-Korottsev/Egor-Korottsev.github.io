dd6de8cd8589d7ed11a078dfffa09847c6de1f1c
Egor-Korottsev <korotzev.egor@gmail.com>
2020-02-21 19:07:50 +0700

    Added js for .where_trip.

--- children_holidays/js/script.js
+++ children_holidays/js/script.js
@@ -41,12 +41,67 @@ $(document).ready(function() {
 
 //.where_trip
 // изменение цвета при наведении.
+var tripMainElement = document.getElementById('where_trip_choices_regions');
+
 var tripElement1 = document.getElementById('where_trip_choices_region-1');
 var tripElement2 = document.getElementById('where_trip_choices_region-2');
 var tripElement3 = document.getElementById('where_trip_choices_region-3');
 
-console.log(tripElement1);
+const defaultColor = "#001e28";
+const changedColor = "white";
+const defaultBackground = "#d8f0f0";
+const changedBackground = "orange"
+const defaultBorderBottom = "0px";
+const changedBorderBottom = "2px solid orange";
+const defaultMarginBottom = "40px";
+const changedMarginBottom = "37.8px";
 
-tripElement1.addEventListener('hover', function (event) {
- console.log(event.type + ' got fired');
-})
+function setSettingForElement_TripMainElement(s) {
+	if(s == 'mouseover') {
+		tripMainElement.style.borderBottom = '2px solid orange';
+ 		tripMainElement.style.paddingBottom = "0.2px";
+ 		tripMainElement.style.marginBottom = "37.8px";
+	} else {
+		tripMainElement.style.borderBottom = '0';
+ 		tripMainElement.style.marginBottom = "40px";
+	}
+}
+
+/*Изменение цвета при наведении для 1-ого элемента.*/
+tripElement1.addEventListener('mouseover', function (event) {
+ 	this.style.background = changedBackground;
+ 	this.style.color = changedColor;
+ 	setSettingForElement_TripMainElement(event.type);
+});
+
+tripElement1.addEventListener('mouseout', function (event) {
+ 	this.style.background = defaultBackground;
+ 	this.style.color = defaultColor;
+ 	setSettingForElement_TripMainElement(event.type);
+});
+
+
+tripElement2.addEventListener('mouseover', function (event) {
+ 	this.style.background = changedBackground;
+ 	this.style.color = changedColor;
+ 	setSettingForElement_TripMainElement(event.type);
+});
+
+tripElement2.addEventListener('mouseout', function (event) {
+ 	this.style.background = defaultBackground;
+ 	this.style.color = defaultColor;
+ 	setSettingForElement_TripMainElement(event.type);
+});
+
+
+tripElement3.addEventListener('mouseover', function (event) {
+ 	this.style.background = changedBackground;
+ 	this.style.color = changedColor; 
+ 	setSettingForElement_TripMainElement(event.type);
+});
+
+tripElement3.addEventListener('mouseout', function (event) {
+ 	this.style.background = defaultBackground;
+ 	this.style.color = defaultColor;
+ 	setSettingForElement_TripMainElement(event.type);
+});
